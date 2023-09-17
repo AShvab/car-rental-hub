7f20ae1fd968a075a0b46e8f6c6caa2d59b65c8b
AShvab <shvabovska@gmail.com>
2023-09-17 23:22:51 +0300

    fix favorite

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import CarItem from "../../components/CarItem/CarItem";
 import { List } from "./FavoritesPage.styled";
 
-
 const FavoritesPage = () => {
   const [favoriteCars, setFavoriteCars] = useState([]);
 
@@ -11,11 +10,24 @@ const FavoritesPage = () => {
     setFavoriteCars(favorites);
   }, []);
 
+  const handleRemoveFromFavorites = (carId) => {
+    
+    const updatedFavorites = favoriteCars.filter((car) => car.id !== carId);
+    setFavoriteCars(updatedFavorites);
+
+
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
+
   return (
     <div>
       <List>
         {favoriteCars.map((car) => (
-          <CarItem key={car.id} car={car} />
+          <CarItem
+            key={car.id}
+            car={car}
+            onRemoveFromFavorites={handleRemoveFromFavorites}
+          />
         ))}
       </List>
     </div>
